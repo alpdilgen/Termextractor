a47a692448696dbe25461b7037f5cd63a9b067e8
alpdilgen <info@anova.bg>
2025-10-24 20:54:03 +0300

    Update __init__.py

--- src/extraction/__init__.py
+++ src/extraction/__init__.py
@@ -1,14 +1,18 @@
+# src/extraction/__init__.py
+
 """Extraction modules for terminology extraction."""
 
-# FIXED: Ensure relative imports start with '.'
-from .term_extractor import TermExtractor, ExtractionResult
+from .term_extractor import TermExtractor
 from .domain_classifier import DomainClassifier, DomainResult
 from .language_processor import LanguageProcessor
+# Make sure this line exists and is correct:
+from .data_models import Term, ExtractionResult
 
 __all__ = [
     "TermExtractor",
-    "ExtractionResult",
     "DomainClassifier",
     "DomainResult",
     "LanguageProcessor",
+    "Term", # Make sure "Term" is listed here
+    "ExtractionResult", # Make sure "ExtractionResult" is listed here
 ]
