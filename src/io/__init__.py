92cf6e6fd9beed80cd339afe51ecf0c53e296507
alpdilgen <info@anova.bg>
2025-10-24 15:54:20 +0300

    Update __init__.py

--- src/io/__init__.py
+++ src/io/__init__.py
@@ -1,7 +1,7 @@
 """I/O modules for file parsing and export."""
 
-from termextractor.io.file_parser import FileParser
-from termextractor.io.format_exporter import FormatExporter
+from .file_parser import FileParser
+from .format_exporter import FormatExporter
 
 __all__ = [
     "FileParser",
